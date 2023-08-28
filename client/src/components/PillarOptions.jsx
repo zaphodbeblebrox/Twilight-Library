92e6171182a9cf43f96c11533d9f973f9cb38f97
zaphodbeblebrox <withrobm@gmail.com>
2023-08-28 16:10:31 -0700

    initial PillarOptions complete
    needs to format based on json in future update.

--- client/src/components/PillarOptions.jsx
+++ client/src/components/PillarOptions.jsx
@@ -1,5 +1,22 @@
+import { Checkbox } from "@radix-ui/themes";
+
 const PillarOptions = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Checkbox value={"Arc Survivors"}>Arc Survivors - 3 Pillars</Checkbox>
+      <Checkbox value={"Characters"}>Characters - 1 Pillar</Checkbox>
+      <div>
+        <p>Encounters - 2 Pillars</p>
+        <Checkbox value={"Bone Eaters"}>Bone Eaters</Checkbox>
+      </div>
+      <Checkbox value={"Scouts"}>Scouts - 1 Pillar</Checkbox>
+      <Checkbox value={"Seed Patterns"}>Seed Patterns - 1 Pillar</Checkbox>
+      <div>
+        <p>Wanderers - 1 Pillar</p>
+        <Checkbox value={"Luck the Wanderer"}>Luck the Wanderer</Checkbox>
+      </div>
+    </div>
+  );
 };
 
 export default PillarOptions;
