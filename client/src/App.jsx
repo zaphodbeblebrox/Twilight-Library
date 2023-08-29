3c33641fafe3e6e176ceb99a67e0110a81186ec7
Brandon Withrow <withrobm@gmail.com>
2023-08-28 21:52:13 -0700

    modified settings to stop the screaming
    referring to eslint and prettier

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,23 @@
-import "./App.css";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import CampaignCreator from "./components/CampaignCreator";
+import './App.css';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import CampaignCreator from './components/CampaignCreator';
 //import JsonStuff from "./static_data/card_database.json";
 
 function App() {
-  // console.log(
-  //   JSON.stringify(
-  //     Object.fromEntries(Object.entries(JsonStuff.knowledge).sort()),
-  //   ),
-  // );
+    // console.log(
+    //   JSON.stringify(
+    //     Object.fromEntries(Object.entries(JsonStuff.knowledge).sort()),
+    //   ),
+    // );
 
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <CampaignCreator />,
-    },
-  ]);
+    const router = createBrowserRouter([
+        {
+            path: '/',
+            element: <CampaignCreator />,
+        },
+    ]);
 
-  return <RouterProvider router={router} />;
+    return <RouterProvider router={router} />;
 }
 
 export default App;
--- client/src/components/CampaignCreator.jsx
+++ client/src/components/CampaignCreator.jsx
@@ -1,49 +1,64 @@
-import PillarOptions from "./PillarOptions";
-import OptionListCC from "./OptionListCC";
-import CcData from "../static_data/campaign_creator.json";
-import RbListCC from "./RbListCC";
-import { Button } from "@radix-ui/themes";
+import PillarOptions from './PillarOptions';
+import OptionListCC from './OptionListCC';
+import CcData from '../static_data/campaign_creator.json';
+import RbListCC from './RbListCC';
+import { Button } from '@radix-ui/themes';
 
 const CampaignCreator = () => {
-  const CreateTimelineHandler = (e) => {
-    e.preventDefault();
-  };
-  return (
-    <form onSubmit={CreateTimelineHandler}>
-      <h2>Campaign Creator</h2>
-      <h3>Campaign Pillars</h3>
-      <PillarOptions />
-      <div>
-        <h3>Node Quarries</h3>
-        <div>
-          <OptionListCC header="NQ1" options={CcData.node_quarry_1} />
-          <OptionListCC header="NQ2" options={CcData.node_quarry_2} />
-          <OptionListCC header="NQ3" options={CcData.node_quarry_3} />
-          <OptionListCC header={"NQ4"} options={CcData.node_quarry_4} />
-        </div>
-      </div>
-      <div>
-        <h3>Node Nemesis</h3>
-        <div>
-          <OptionListCC header="NN1" options={CcData.node_nemesis_1} />
-          <OptionListCC header="NN2" options={CcData.node_nemesis_2} />
-          <OptionListCC header="NN3" options={CcData.node_nemesis_3} />
-        </div>
-      </div>
-      <div>
-        <h3>Node Critical</h3>
-        <div>
-          <RbListCC header="Core" options={CcData.node_core} />
-          <RbListCC header="Finale" options={CcData.node_finale} />
-          <OptionListCC header="Special" options={CcData.node_special} />
-        </div>
-      </div>
-      <div>
-        <Button>Cancel</Button>
-        <Button>Timeline</Button>
-      </div>
-    </form>
-  );
+    const CreateTimelineHandler = (e) => {
+        e.preventDefault();
+    };
+    return (
+        <form onSubmit={CreateTimelineHandler}>
+            <h2>Campaign Creator</h2>
+            <h3>Campaign Pillars</h3>
+            <PillarOptions />
+            <div>
+                <h3>Node Quarries</h3>
+                <div>
+                    <OptionListCC header="NQ1" options={CcData.node_quarry_1} />
+                    <OptionListCC header="NQ2" options={CcData.node_quarry_2} />
+                    <OptionListCC header="NQ3" options={CcData.node_quarry_3} />
+                    <OptionListCC
+                        header={'NQ4'}
+                        options={CcData.node_quarry_4}
+                    />
+                </div>
+            </div>
+            <div>
+                <h3>Node Nemesis</h3>
+                <div>
+                    <OptionListCC
+                        header="NN1"
+                        options={CcData.node_nemesis_1}
+                    />
+                    <OptionListCC
+                        header="NN2"
+                        options={CcData.node_nemesis_2}
+                    />
+                    <OptionListCC
+                        header="NN3"
+                        options={CcData.node_nemesis_3}
+                    />
+                </div>
+            </div>
+            <div>
+                <h3>Node Critical</h3>
+                <div>
+                    <RbListCC header="Core" options={CcData.node_core} />
+                    <RbListCC header="Finale" options={CcData.node_finale} />
+                    <OptionListCC
+                        header="Special"
+                        options={CcData.node_special}
+                    />
+                </div>
+            </div>
+            <div>
+                <Button>Cancel</Button>
+                <Button>Timeline</Button>
+            </div>
+        </form>
+    );
 };
 
 export default CampaignCreator;
