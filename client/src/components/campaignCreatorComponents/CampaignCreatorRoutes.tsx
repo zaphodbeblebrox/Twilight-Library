ac6bb20ea1f094ae2134bc561024d363afbd3402
zaphodbeblebrox <withrobm@gmail.com>
2023-09-20 16:05:53 -0700

    Moved PresetCampaign json config to its own file

--- client/src/components/campaignCreatorComponents/CampaignCreatorRoutes.tsx
+++ client/src/components/campaignCreatorComponents/CampaignCreatorRoutes.tsx
@@ -4,41 +4,7 @@ import TimelineCreator from './TimelineCreator';
 import ccData from '../../static_data/campaign_creator.json';
 import presetCampaignData from '../../static_data/preset_campaigns.json';
 import { useState } from 'react';
-
-type TypeCampaignData = {
-    node_quarry_1: string[];
-    node_quarry_2: string[];
-    node_quarry_3: string[];
-    node_quarry_4: string[];
-    node_nemesis_1: string[];
-    node_nemesis_2: string[];
-    node_nemesis_3: string[];
-    node_core: string;
-    node_finale: string | null;
-    node_special: string[];
-    encounters: string[];
-    wanderers: string[];
-    pillars: string[];
-    milestones: { trigger: string; event: string }[];
-    flexible_nemesis_encounters: boolean;
-    nn1_lvl1_fight_year: number | null;
-    nn1_lvl2_fight_year: number | null;
-    nn1_lvl3_fight_year: number | null;
-    nn2_lvl1_fight_year: number | null;
-    nn2_lvl2_fight_year: number | null;
-    nn2_lvl3_fight_year: number | null;
-    nn3_lvl1_fight_year: number | null;
-    nn3_lvl2_fight_year: number | null;
-    nn3_lvl3_fight_year: number | null;
-    core_fight_year: number;
-    finale_fight_year: number | null;
-    timeline: Record<string, string[]>;
-    courage_event_1: string;
-    courage_event_2: string;
-    understanding_event_1: string;
-    understanding_event_2: string;
-    constellations: boolean;
-};
+import { TypeCampaignData } from './CampaignTypeConfig';
 
 function CampaignCreatorRoutes() {
     const intializeDefaultCampaign = () => {
@@ -64,28 +30,27 @@ function CampaignCreatorRoutes() {
         ccData.encounters.forEach((key: string) => (dataHolder[key] = false));
         ccData.wanderers.forEach((key: string) => (dataHolder[key] = false));
 
-        const defaultCampaign: TypeCampaignData = intializeDefaultCampaign();
-
-        defaultCampaign.node_quarry_1.forEach((key: string) => (dataHolder[key] = true));
-        defaultCampaign.node_quarry_2.forEach((key: string) => (dataHolder[key] = true));
-        defaultCampaign.node_quarry_3.forEach((key: string) => (dataHolder[key] = true));
-        defaultCampaign.node_quarry_4.forEach((key: string) => (dataHolder[key] = true));
-        defaultCampaign.node_nemesis_1.forEach((key: string) => (dataHolder[key] = true));
-        defaultCampaign.node_nemesis_2.forEach((key: string) => (dataHolder[key] = true));
-        defaultCampaign.node_nemesis_3.forEach((key: string) => (dataHolder[key] = true));
-        dataHolder[defaultCampaign.node_core] = true;
-        if (defaultCampaign.node_finale !== null) {
-            dataHolder[defaultCampaign.node_finale] = true;
+        selectedCampaign.node_quarry_1.forEach((key: string) => (dataHolder[key] = true));
+        selectedCampaign.node_quarry_2.forEach((key: string) => (dataHolder[key] = true));
+        selectedCampaign.node_quarry_3.forEach((key: string) => (dataHolder[key] = true));
+        selectedCampaign.node_quarry_4.forEach((key: string) => (dataHolder[key] = true));
+        selectedCampaign.node_nemesis_1.forEach((key: string) => (dataHolder[key] = true));
+        selectedCampaign.node_nemesis_2.forEach((key: string) => (dataHolder[key] = true));
+        selectedCampaign.node_nemesis_3.forEach((key: string) => (dataHolder[key] = true));
+        dataHolder[selectedCampaign.node_core] = true;
+        if (selectedCampaign.node_finale !== null) {
+            dataHolder[selectedCampaign.node_finale] = true;
         }
-        defaultCampaign.node_special.forEach((key: string) => (dataHolder[key] = true));
-        defaultCampaign.pillars.forEach((key: string) => (dataHolder[key] = true));
-        defaultCampaign.encounters.forEach((key: string) => (dataHolder[key] = true));
-        defaultCampaign.wanderers.forEach((key: string) => (dataHolder[key] = true));
+        selectedCampaign.node_special.forEach((key: string) => (dataHolder[key] = true));
+        selectedCampaign.pillars.forEach((key: string) => (dataHolder[key] = true));
+        selectedCampaign.encounters.forEach((key: string) => (dataHolder[key] = true));
+        selectedCampaign.wanderers.forEach((key: string) => (dataHolder[key] = true));
 
         return dataHolder;
     };
+
     const [selectedCampaign, setSelectedCampaign] = useState(intializeDefaultCampaign);
-    const [timeline, setTimeline] = useState([]);
+    const [timeline, setTimeline] = useState({});
     const [campaignSettings, setCampaignSettings] = useState(initializeSettings);
     const [settlementName, setSettlementName] = useState('');
 
@@ -99,6 +64,11 @@ function CampaignCreatorRoutes() {
                         setSettlementName={setSettlementName}
                         campaignSettings={campaignSettings}
                         setCampaignSettings={setCampaignSettings}
+                        selectedCampaign={selectedCampaign}
+                        setSelectedCampaign={setSelectedCampaign}
+                        handleCampaignChange={initializeSettings}
+                        timeline={timeline}
+                        setTimeline={setTimeline}
                     />
                 }
             />
---
+++ client/src/components/campaignCreatorComponents/CampaignTypeConfig.tsx
@@ -0,0 +1,41 @@
+import presetCampaignsData from '../../static_data/preset_campaigns.json';
+
+export type NodePillarLists = {
+    node_quarry_1: string[];
+    node_quarry_2: string[];
+    node_quarry_3: string[];
+    node_quarry_4: string[];
+    node_nemesis_1: string[];
+    node_nemesis_2: string[];
+    node_nemesis_3: string[];
+    node_special: string[];
+    encounters: string[];
+    wanderers: string[];
+    pillars: string[];
+};
+export type NemesisFightYearLists = {
+    nn1_lvl1_fight_year: number | null;
+    nn1_lvl2_fight_year: number | null;
+    nn1_lvl3_fight_year: number | null;
+    nn2_lvl1_fight_year: number | null;
+    nn2_lvl2_fight_year: number | null;
+    nn2_lvl3_fight_year: number | null;
+    nn3_lvl1_fight_year: number | null;
+    nn3_lvl2_fight_year: number | null;
+    nn3_lvl3_fight_year: number | null;
+    finale_fight_year: number | null;
+};
+export type TypeCampaignData = NodePillarLists &
+    NemesisFightYearLists & {
+        node_core: string;
+        node_finale: string | null;
+        milestones: { trigger: string; event: string }[];
+        flexible_nemesis_encounters: boolean;
+        core_fight_year: number;
+        timeline: Record<string, string[]>;
+        courage_event_1: string;
+        courage_event_2: string;
+        understanding_event_1: string;
+        understanding_event_2: string;
+        constellations: boolean;
+    };
