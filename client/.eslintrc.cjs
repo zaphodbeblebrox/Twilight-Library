b1d6c2b1a69029fc3f226b6c38109bb26ca11d68
zaphodbeblebrox <withrobm@gmail.com>
2023-11-17 09:41:42 -0800

    Revert "attempt to fix prettier spacing issue with ternary. No dice."
    This reverts commit 1644dffa6b039cf034bf03075330e97e5200be1c.

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -7,7 +7,6 @@ module.exports = {
         'plugin:react/jsx-runtime',
         'plugin:react-hooks/recommended',
         'plugin:prettier/recommended',
-        'prettier'
     ],
     ignorePatterns: ['dist', '.eslintrc.cjs'],
     parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
@@ -18,6 +17,5 @@ module.exports = {
             'warn',
             { allowConstantExport: true },
         ],
-        "indent": "off"
     },
 };
