62911c45aeb772b569fca70e1c7e4bf8fbd8189f
zaphodbeblebrox <withrobm@gmail.com>
2023-11-26 17:27:27 -0800

    got initial fuze search working
    displaying only in console atm.

--- client/src/components/campaignDisplay/GearTab.tsx
+++ client/src/components/campaignDisplay/GearTab.tsx
@@ -4,6 +4,8 @@ import { TypeServerSettlement } from '../../../../SettlementTypes';
 import { settlementApi } from '../../service/api';
 import { TwilightEditCountDialog } from '../primitiveComponents/AlertBoxes';
 import { TwilightNodeHeader } from '../primitiveComponents/Primitives';
+import { TwilightSearchPopup } from '../primitiveComponents/SearchBoxes';
+import { gearData } from '../static_data_file_configs/gearConfig';
 
 interface GearTabProps {
     campaignData: TypeServerSettlement;
@@ -18,6 +20,9 @@ const GearTab = ({ campaignData, dbRefetch }: GearTabProps) => {
         },
         { manual: true },
     );
+
+    const handleAddGear = () => {};
+
     return (
         <Flex direction="row" wrap="wrap" gap="3">
             {Object.keys(campaignData.gear)
@@ -25,6 +30,12 @@ const GearTab = ({ campaignData, dbRefetch }: GearTabProps) => {
                 .map((gearCategory, idx) => {
                     return (
                         <Flex key={idx} direction="column" gap="1" justify="start" align="end">
+                            <TwilightSearchPopup
+                                buttonText="Add Gear"
+                                labelText="Search Gear"
+                                options={Object.keys(gearData)}
+                                onSubmit={handleAddGear}
+                            />
                             <TwilightNodeHeader headerText={gearCategory} />
                             {Object.keys(campaignData.gear[gearCategory])
                                 .sort()
--- client/src/components/primitiveComponents/SearchBoxes.tsx
+++ client/src/components/primitiveComponents/SearchBoxes.tsx
@@ -1,7 +1,8 @@
 import * as Label from '@radix-ui/react-label';
 import { AlertDialog, Button, Dialog, Flex, Text, TextField } from '@radix-ui/themes';
-import { useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { TwilightSelect } from './Primitives';
+import Fuse from 'fuse.js';
 
 interface TwilightSearchPopup {
     buttonText: string;
@@ -11,10 +12,19 @@ interface TwilightSearchPopup {
 }
 
 const TwilightSearchPopup = ({ buttonText, labelText, options, onSubmit }: TwilightSearchPopup) => {
-    const [selection, setSelection] = useState('');
+    const [searchTerm, setSearchTerm] = useState('');
+    const [results, setResults] = useState<{}[]>([]);
+
+    const settings = {
+        includeScore: true,
+        threshold: 0.2,
+    };
+
+    const fuse = useMemo(() => new Fuse(options, settings), []);
+    useEffect(() => console.log(results), [results]);
 
     return (
-        <Dialog.Root onOpenChange={() => setSelection('')}>
+        <Dialog.Root onOpenChange={() => setSearchTerm('')}>
             <Dialog.Trigger>
                 <Button>{buttonText}</Button>
             </Dialog.Trigger>
@@ -23,15 +33,53 @@ const TwilightSearchPopup = ({ buttonText, labelText, options, onSubmit }: Twili
                 <Dialog.Title className="DialogTitle">{labelText}</Dialog.Title>
 
                 <Dialog.Description className="DialogDescription">Search:</Dialog.Description>
-
-                <Flex direction="column" justify="center" align="center" gap="3">
-                    <Button onClick={() => setCurrentCount((newCount) => newCount + 1)}>+</Button>
-                    <Text size="2">{currentCount}</Text>
-                    <Button onClick={() => currentCount > 0 && setCurrentCount((newCount) => newCount - 1)}>-</Button>
+                <Flex direction="column" gap="3">
+                    <TextField.Input
+                        placeholder="Find gear..."
+                        value={searchTerm}
+                        onChange={(e) => {
+                            const nextSearchTerm = e.currentTarget.value;
+                            setSearchTerm(nextSearchTerm);
+                            setResults(
+                                fuse.search(nextSearchTerm),
+                                // .map(({ item }) => getCharacter(item.id as Role)),
+                            );
+                        }}
+                    />
+                    {/* {results.map((role) => (
+                        <Dialog.Close key={role.id}>
+                            <button
+                                onClick={async () => {
+                                    if (!script.find((r) => r.id === role.id)) {
+                                        await setScript([...script, { id: role.id as Role }]);
+                                    }
+                                    if (!characterSelectState.selectedRoles.value[role.id]) {
+                                        characterSelectState.selectedRoles.set((selectedRoles) => ({
+                                            ...selectedRoles,
+                                            [role.id]: 1,
+                                        }));
+                                    }
+                                }}
+                            >
+                                <Flex justify="between" align="center">
+                                    <Flex gap="2" align="center">
+                                        <RoleIcon role={role.id} className="h-[60px]" />
+                                        <Text size="5" autoCapitalize="true">
+                                            {RoleName(role.id)}
+                                        </Text>
+                                    </Flex>
+                                    <Text size="5" autoCapitalize="true">
+                                        {role.team}
+                                    </Text>
+                                </Flex>
+                            </button>
+                        </Dialog.Close>
+                    ))} */}
                 </Flex>
+
                 <Dialog.Close>
-                    <Button onClick={() => onSubmit(currentCount)} variant="solid" color="green">
-                        Update
+                    <Button variant="solid" color="green">
+                        Add
                     </Button>
                 </Dialog.Close>
             </Dialog.Content>
