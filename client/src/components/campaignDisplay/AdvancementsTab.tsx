8bee960d7de619ad9c6e13f60a94a5109fe346c6
zaphodbeblebrox <withrobm@gmail.com>
2023-12-05 15:23:25 -0800

    Create AdvancementsTab.tsx

---
+++ client/src/components/campaignDisplay/AdvancementsTab.tsx
@@ -0,0 +1,45 @@
+import { Box, Flex, Tabs, Text } from '@radix-ui/themes';
+import useAxios, { RefetchFunction } from 'axios-hooks';
+import { TypeServerSettlement } from '../../../../SettlementTypes';
+import { settlementApi } from '../../service/api';
+import { TwilightEditCountDialog } from '../primitiveComponents/AlertBoxes';
+import { TwilightNodeHeader } from '../primitiveComponents/Primitives';
+
+interface AdvancementsTabProps {
+    campaignData: TypeServerSettlement;
+    dbRefetch: RefetchFunction<any, any>;
+    dbExecutePatch: RefetchFunction<any, any>;
+}
+
+const AdvancementsTab = ({ campaignData, dbRefetch, dbExecutePatch }: AdvancementsTabProps) => {
+    return (
+        <Tabs.Root
+            defaultValue="resources"
+            style={{ display: 'flex', justifyContent: 'start', flexDirection: 'column', flex: 1, width: '100%' }}
+        >
+            <Tabs.List>
+                <Tabs.Trigger style={{ flex: 1 }} value="innovations">
+                    Innovations
+                </Tabs.Trigger>
+                <Tabs.Trigger style={{ flex: 1 }} value="principles">
+                    Principles
+                </Tabs.Trigger>
+                <Tabs.Trigger style={{ flex: 1 }} value="milestones">
+                    Milestones
+                </Tabs.Trigger>
+                <Tabs.Trigger style={{ flex: 1 }} value="bonuses">
+                    Bonuses
+                </Tabs.Trigger>
+            </Tabs.List>
+
+            <Box px="4" pt="3" pb="2">
+                <Tabs.Content value="innovations">{campaignData && <Text>...Loading innovations</Text>}</Tabs.Content>
+                <Tabs.Content value="principles">{campaignData && <Text>...Loading principles</Text>}</Tabs.Content>
+                <Tabs.Content value="milestones">{campaignData && <Text>...Loading milestones</Text>}</Tabs.Content>
+                <Tabs.Content value="bonuses">{campaignData && <Text>...Loading bonuses</Text>}</Tabs.Content>
+            </Box>
+        </Tabs.Root>
+    );
+};
+
+export default AdvancementsTab;
