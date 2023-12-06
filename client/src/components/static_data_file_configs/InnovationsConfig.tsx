16e7316fdb6d006301268eaf24e4030d3ff1d178
zaphodbeblebrox <withrobm@gmail.com>
2023-12-05 16:57:07 -0800

    Create InnovationsConfig.tsx
    created base document, not implemented!
    
    Added full layout of data object

---
+++ client/src/components/static_data_file_configs/InnovationsConfig.tsx
@@ -0,0 +1,54 @@
+import rawInnovationsData from '../../static_data/innovations.json';
+
+{
+    "name": ,
+    "static_effect": [],
+    "survival_limit_mod": ,
+    "departing_survival": ,
+    "departing_insanity": ,
+    "mod_strength": ,
+    "mod_accuracy": ,
+    "mod_luck": ,
+    "mod_speed": ,
+    "mod_lumi": ,
+    "mod_evasion": ,
+    "mod_movement": ,
+    "endeavor":[{
+        "cost": ,
+        "roll": ,
+        "table": [{
+            "limit_low": ,
+            "limit_high": ,
+            "effect": 
+        }]
+    }]
+}
+
+// --- Innovations Json ---
+export type TypeAffinities = {
+    top: string;
+    right: string;
+    bottom: string;
+    left: string;
+    center: string;
+};
+
+export type TypeStats = {
+    armor: number;
+    location: string;
+    speed: number | string;
+    accuracy: number;
+    strength: number | string;
+};
+
+export type TypeGearData = {
+    types: string[];
+    location: string;
+    crafting: string;
+    stats: Partial<TypeStats>;
+    affinities: Record<keyof TypeAffinities, string>;
+    abilities: string[];
+};
+
+export type GearKeys = { [K in keyof typeof rawGearData]: string[] };
+export const gearData: Record<keyof GearKeys, TypeGearData> = rawGearData;
