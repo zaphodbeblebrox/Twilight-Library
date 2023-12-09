2afdd3c622763d42fda37cfe666e85d53de9cbe1
zaphodbeblebrox <withrobm@gmail.com>
2023-12-08 18:29:53 -0800

    Update InnovationsConfig.tsx
    fully implemented type breakdown

--- client/src/components/static_data_file_configs/InnovationsConfig.tsx
+++ client/src/components/static_data_file_configs/InnovationsConfig.tsx
@@ -1,58 +1,67 @@
 import rawInnovationsData from '../../static_data/innovations.json';
 
-{
-    "name": "",
-    "static_effect": [""],
-    "survival_limit_mod": ,
-    "departing_survival": ,
-    "departing_insanity": ,
-    "mod_strength": ,
-    "mod_accuracy": ,
-    "mod_luck": ,
-    "mod_speed": ,
-    "mod_lumi": ,
-    "mod_evasion": ,
-    "mod_movement": ,
-    "mod_understanding": ,
-    "mod_courage": ,
-    "mod_hunt_xp": ,
-    "endeavor":[{
-        "cost": "",
-        "effect": "",
-        "roll": "",
-        "table": [{
-            "limit_low": "",
-            "limit_high": "",
-            "effect": ""
-        }]
-    }]
-}
+// {
+//     "name": str,
+//     "static_effect": str[],
+//     "survival_limit_mod": num,
+//     "departing_survival": num,
+//     "departing_insanity": num,
+//     "mod_strength": num,
+//     "mod_accuracy": num,
+//     "mod_luck": num,
+//     "mod_speed": num,
+//     "mod_lumi": num,
+//     "mod_evasion": num,
+//     "mod_movement": num,
+//     "mod_understanding": num,
+//     "mod_courage": num,
+//     "mod_hunt_xp": num,
+//     "endeavor":[{
+//         "cost": str,
+//         "effect": str,
+//         "roll": str,
+//         "table": [{
+//             "limit_low": str,
+//             "limit_high": str,
+//             "effect": str
+//         }]
+//     }]
+// }
 
 // --- Innovations Json ---
-export type TypeAffinities = {
-    top: string;
-    right: string;
-    bottom: string;
-    left: string;
-    center: string;
+export type TypeInnovationStatMods = {
+    survival_limit_mod: number;
+    departing_survival: number;
+    departing_insanity: number;
+    mod_strength: number;
+    mod_accuracy: number;
+    mod_luck: number;
+    mod_speed: number;
+    mod_lumi: number;
+    mod_evasion: number;
+    mod_movement: number;
+    mod_understanding: number;
+    mod_courage: number;
+    mod_hunt_xp: number;
 };
 
-export type TypeStats = {
-    armor: number;
-    location: string;
-    speed: number | string;
-    accuracy: number;
-    strength: number | string;
+export type TypeInnovationEndeavorTable = {
+    limit_low: string;
+    limit_high: string;
+    effect: string;
 };
 
-export type TypeGearData = {
-    types: string[];
-    location: string;
-    crafting: string;
-    stats: Partial<TypeStats>;
-    affinities: Record<keyof TypeAffinities, string>;
-    abilities: string[];
+export type TypeInnovationEndeavor = {
+    cost: string;
+    effect: string;
+    roll: string;
+    table: TypeInnovationEndeavorTable[];
 };
 
-export type GearKeys = { [K in keyof typeof rawGearData]: string[] };
-export const gearData: Record<keyof GearKeys, TypeGearData> = rawGearData;
+export type TypeInnovationData = Partial<TypeInnovationStatMods> & {
+    name: string;
+    static_effect?: string[];
+    endeavor?: Partial<TypeInnovationEndeavor>[];
+};
+
+export const innovationData: TypeInnovationData[] = rawInnovationsData;
