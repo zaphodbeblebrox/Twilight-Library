8f4976f684a4eb9d2b229900d14d0967e08b7a5e
zaphodbeblebrox <withrobm@gmail.com>
2023-12-22 20:46:30 -0800

    Create Principle.tsx

---
+++ client/src/components/CampaignCreator/TriggerEvents/Principle.tsx
@@ -0,0 +1,33 @@
+import { Flex, Text } from '@radix-ui/themes';
+import { PrinciplesDefaultLists, TypeCampaignData } from '../../static_data_file_configs/PresetCampaignConfig';
+import ModifyPrinciple from './ModifyPrinciple';
+
+interface PrincipleProps {
+    principleLable: string;
+    principlekey: keyof PrinciplesDefaultLists;
+    campaignSettings: TypeCampaignData;
+    setCampaignSettings: React.Dispatch<React.SetStateAction<TypeCampaignData>>;
+}
+
+const Principle = ({ principleLable, principlekey, campaignSettings, setCampaignSettings }: PrincipleProps) => {
+    const principleObj = campaignSettings[principlekey];
+    return (
+        <Flex direction="row" gap="3" align="center">
+            <Text>Principle - {principleLable}:</Text>
+            {Object.keys(principleObj).map((option, idx) => {
+                return (
+                    <ModifyPrinciple
+                        key={idx}
+                        buttonText={principleObj[option as keyof typeof principleObj]}
+                        targetKey={option}
+                        onSubmit={(selectedOption) => {
+                            console.log(selectedOption);
+                        }}
+                    />
+                );
+            })}
+        </Flex>
+    );
+};
+
+export default Principle;
