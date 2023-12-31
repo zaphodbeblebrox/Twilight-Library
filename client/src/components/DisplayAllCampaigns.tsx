bf28d3c4f9cbd70a7f528d57c9a9f3bba67a1dc2
zaphodbeblebrox <withrobm@gmail.com>
2023-12-30 20:51:11 -0800

    code cleanup

--- client/src/components/DisplayAllCampaigns.tsx
+++ client/src/components/DisplayAllCampaigns.tsx
@@ -2,34 +2,12 @@ import { Button, Flex } from '@radix-ui/themes';
 import { useNavigate } from 'react-router-dom';
 import { settlementApi } from '../service/api';
 import useAxios, { configure } from 'axios-hooks';
-import { useEffect, useState } from 'react';
-// import { useIsFocused } from '@react-navigation/native';
 
 const DisplayAllCampaigns = () => {
-    // const [axiosInitiated, setAxiosInitiated] = useState(false);
     const [{ data, loading, error }, refetch] = useAxios(`${settlementApi}/get`, { useCache: false });
     configure;
 
     const navigate = useNavigate();
-
-    // const isFocused = useIsFocused();
-
-    // useEffect(() => {
-    //     if (isFocused) {
-    //         refetch();
-    //     }
-    // }, [isFocused, refetch]);
-
-    // TODO: Trigger refetch() when navigated to.
-
-    useEffect(() => {
-        console.log('Mounted!');
-        console.log('Data: ', data);
-        console.log('Loading: ', loading);
-        console.log('Error: ', error);
-        // console.log('DAC has Remounted!', axiosInitiated);
-        // axiosInitiated ? refetch() : setAxiosInitiated(true);
-    }, [loading]);
 
     if (loading) {
         return <p>Loading...</p>;
