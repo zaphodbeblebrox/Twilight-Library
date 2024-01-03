a355c65a74f3e17392845f9ebbafb27d9ffded07
zaphodbeblebrox <withrobm@gmail.com>
2024-01-03 00:18:28 -0800

    Update index.tsx

--- client/src/components/CampaignCreator/MonstersAndModules/InitializeTimeline/index.tsx
+++ client/src/components/CampaignCreator/MonstersAndModules/InitializeTimeline/index.tsx
@@ -17,36 +17,36 @@ const InitializeTimeline = (campaignSettings: TypeCampaignData) => {
 
     // Populate Timeline
     const monsterTimelineFunctionArray = [
-        (tl: Record<number, string[]>) =>
+        (tl: Record<number, TypeYear>) =>
             TransformData.AddMonsterEvents(campaignSettings, tl, 'node_quarry_1', 'quarries'),
-        (tl: Record<number, string[]>) =>
+        (tl: Record<number, TypeYear>) =>
             TransformData.AddMonsterEvents(campaignSettings, tl, 'node_quarry_2', 'quarries'),
-        (tl: Record<number, string[]>) =>
+        (tl: Record<number, TypeYear>) =>
             TransformData.AddMonsterEvents(campaignSettings, tl, 'node_quarry_3', 'quarries'),
-        (tl: Record<number, string[]>) =>
+        (tl: Record<number, TypeYear>) =>
             TransformData.AddMonsterEvents(campaignSettings, tl, 'node_quarry_4', 'quarries'),
-        (tl: Record<number, string[]>) => TransformData.AddMonsterEvents(campaignSettings, tl, 'pillars', 'pillars'),
-        (tl: Record<number, string[]>) =>
+        (tl: Record<number, TypeYear>) => TransformData.AddMonsterEvents(campaignSettings, tl, 'pillars', 'pillars'),
+        (tl: Record<number, TypeYear>) =>
             TransformData.AddMonsterEvents(campaignSettings, tl, 'encounters', 'encounters'),
-        (tl: Record<number, string[]>) =>
+        (tl: Record<number, TypeYear>) =>
             TransformData.AddMonsterEvents(campaignSettings, tl, 'wanderers', 'wanderers'),
-        (tl: Record<number, string[]>) =>
+        (tl: Record<number, TypeYear>) =>
             TransformData.AddMonsterEvents(campaignSettings, tl, 'node_nemesis_1', 'nemesis'),
-        (tl: Record<number, string[]>) =>
+        (tl: Record<number, TypeYear>) =>
             TransformData.AddMonsterEvents(campaignSettings, tl, 'node_nemesis_2', 'nemesis'),
-        (tl: Record<number, string[]>) =>
+        (tl: Record<number, TypeYear>) =>
             TransformData.AddMonsterEvents(campaignSettings, tl, 'node_nemesis_3', 'nemesis'),
-        (tl: Record<number, string[]>) => TransformData.AddBossEvents(campaignSettings, tl, 'core'),
-        (tl: Record<number, string[]>) => TransformData.AddBossEvents(campaignSettings, tl, 'finale'),
-        (tl: Record<number, string[]>) => TransformData.AddDefaultEvents(campaignSettings, tl),
+        // (tl: Record<number, TypeYear>) => TransformData.AddBossEvents(campaignSettings, tl, 'core'),
+        // (tl: Record<number, TypeYear>) => TransformData.AddBossEvents(campaignSettings, tl, 'finale'),
+        // (tl: Record<number, TypeYear>) => TransformData.AddDefaultEvents(campaignSettings, tl),
     ];
-    const updatedTimeline: Record<number, string[]> = monsterTimelineFunctionArray.reduce(
+    const updatedTimeline: Record<number, TypeYear> = monsterTimelineFunctionArray.reduce(
         (currentTimeline, monsterEventFunction) => {
             return monsterEventFunction(currentTimeline);
         },
         timeline,
     );
-    console.log('testing timeline:', updatedTimeline);
+    console.log('converted timeline:', updatedTimeline);
     return updatedTimeline;
 };
 
