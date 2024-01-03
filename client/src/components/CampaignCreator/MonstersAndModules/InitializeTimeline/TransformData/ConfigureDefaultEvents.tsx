54bcbdd0f1f9f4a12bf69d423ab933901a872274
zaphodbeblebrox <withrobm@gmail.com>
2024-01-02 21:59:36 -0800

    Update ConfigureDefaultEvents.tsx

--- client/src/components/CampaignCreator/MonstersAndModules/InitializeTimeline/TransformData/ConfigureDefaultEvents.tsx
+++ client/src/components/CampaignCreator/MonstersAndModules/InitializeTimeline/TransformData/ConfigureDefaultEvents.tsx
@@ -1,6 +1,7 @@
+import { TypeYear } from '../../../../../../../SettlementTypes';
 import { TypeCampaignData } from '../../../../static_data_file_configs/PresetCampaignConfig';
 
-const ConfigureDefaultEvents = (campaignSettings: TypeCampaignData, timeline: Record<number, string[]>) => {
+const ConfigureDefaultEvents = (campaignSettings: TypeCampaignData, timeline: Record<number, TypeYear>) => {
     Object.keys(campaignSettings.default_timeline).forEach((yearKey: string) => {
         campaignSettings.default_timeline[Number(yearKey)].forEach((yearData: string) => {
             timeline[Number(yearKey)].push(yearData);
