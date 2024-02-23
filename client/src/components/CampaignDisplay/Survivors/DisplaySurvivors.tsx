a58798146e2bb4bbda2a9812bb75a6ab5c51aca6
zaphodbeblebrox <withrobm@gmail.com>
2024-02-23 00:03:07 -0800

    Update DisplaySurvivors.tsx
    filtered and initiated

--- client/src/components/CampaignDisplay/Survivors/DisplaySurvivors.tsx
+++ client/src/components/CampaignDisplay/Survivors/DisplaySurvivors.tsx
@@ -1,6 +1,7 @@
 import { RefetchFunction } from 'axios-hooks';
 import { TypeServerSettlement } from '../../../../../SettlementTypes';
 import { Box, Flex, Heading, Tabs, Text } from '@radix-ui/themes';
+import Fuse from 'fuse.js';
 
 interface DisplaySurvivorsProps {
     campaignData: TypeServerSettlement;
@@ -15,8 +16,18 @@ const DisplaySurvivors = ({
     dbRefetch,
     dbExecutePatch,
 }: DisplaySurvivorsProps) => {
-    // TODO: create object of only alive or dead survivors using search
+    // create object of only alive or dead survivors using filter
+    const survivorList = campaignData.survivors.filter((survivor) => survivor.is_dead === showDeadSurvivors);
     // TODO: display all survivors in object
+    return (
+        <Flex>
+            {!showDeadSurvivors && <button>New Survivor</button>}
+            {survivorList.map((survivor, idx) => {
+                return <p key={idx}>{survivor.first_name}</p>;
+            })}
+            <p>placeholder</p>
+        </Flex>
+    );
 };
 
 export default DisplaySurvivors;
