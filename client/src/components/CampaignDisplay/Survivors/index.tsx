781cabd87f5e09d495c1ea754f1688287d62ae47
zaphodbeblebrox <withrobm@gmail.com>
2024-02-23 00:48:31 -0800

    DisplaySurvivors now renders

--- client/src/components/CampaignDisplay/Survivors/index.tsx
+++ client/src/components/CampaignDisplay/Survivors/index.tsx
@@ -1,6 +1,7 @@
 import { RefetchFunction } from 'axios-hooks';
 import { TypeServerSettlement } from '../../../../../SettlementTypes';
 import { Box, Flex, Heading, Tabs, Text } from '@radix-ui/themes';
+import DisplaySurvivors from './DisplaySurvivors';
 
 interface TabSurvivorsProps {
     campaignData: TypeServerSettlement;
@@ -27,8 +28,25 @@ const TabSurvivors = ({ campaignData, dbRefetch, dbExecutePatch }: TabSurvivorsP
             </Tabs.List>
 
             <Box px="4" pt="3" pb="2">
-                <Tabs.Content value="alive">{campaignData && <p>survivors - alive</p>}</Tabs.Content>
-                <Tabs.Content value="dead">{campaignData && <p>survivors - dead</p>}</Tabs.Content>
+                <Tabs.Content value="alive">
+                    {campaignData && (
+                        <DisplaySurvivors
+                            campaignData={campaignData}
+                            dbRefetch={dbRefetch}
+                            dbExecutePatch={dbExecutePatch}
+                        />
+                    )}
+                </Tabs.Content>
+                <Tabs.Content value="dead">
+                    {campaignData && (
+                        <DisplaySurvivors
+                            campaignData={campaignData}
+                            dbRefetch={dbRefetch}
+                            dbExecutePatch={dbExecutePatch}
+                            showDeadSurvivors={true}
+                        />
+                    )}
+                </Tabs.Content>
                 <Tabs.Content value="departed">{campaignData && <p>survivors - departed</p>}</Tabs.Content>
             </Box>
         </Tabs.Root>
